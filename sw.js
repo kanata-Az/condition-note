5dab05abe762bd8e5c7cee06f5a043e2e323ef70
kanata-Az <kanata_cat29@yahoo.co.jp>
2026-07-31 22:15:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "condition-note-v240";
+const CACHE = "condition-note-v250";
 const FILES = ["/", "/index.html", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 self.addEventListener("install", event => {
   self.skipWaiting();
