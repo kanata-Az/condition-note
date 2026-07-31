ce546c5a80b65685677d66b7fc358998d600980d
kanata-Az <kanata_cat29@yahoo.co.jp>
2026-07-31 21:37:03 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,50 +1,20 @@
-const CACHE = "condition-note-v2.3.1-cache-1";
-const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
-
+const CACHE = "condition-note-v2.3-cache-1";
+const FILES = ["/", "/index.html", "/manifest.webmanifest", "/icon-192.png", "/icon-512.png"];
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
 });
-
 self.addEventListener("activate", event => {
-  event.waitUntil((async () => {
-    const keys = await caches.keys();
-    await Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)));
-    await self.clients.claim();
-  })());
+  event.waitUntil(Promise.all([
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))),
+    self.clients.claim()
+  ]));
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-
-  if (event.request.mode === "navigate") {
-    event.respondWith((async () => {
-      try {
-        const response = await fetch(event.request, {cache: "no-store"});
-        const cache = await caches.open(CACHE);
-        cache.put("/index.html", response.clone());
-        return response;
-      } catch {
-        return (await caches.match("/index.html")) || (await caches.match("/"));
-      }
-    })());
-    return;
-  }
-
-  event.respondWith((async () => {
-    const sameOrigin = new URL(event.request.url).origin === self.location.origin;
-    if (sameOrigin) {
-      try {
-        const response = await fetch(event.request, {cache: "no-store"});
-        if (response.ok) {
-          const cache = await caches.open(CACHE);
-          cache.put(event.request, response.clone());
-        }
-        return response;
-      } catch {
-        return (await caches.match(event.request)) || Response.error();
-      }
-    }
-    return fetch(event.request);
-  })());
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match("/index.html"))));
 });
